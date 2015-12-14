1e51343591cf9aaae5d728842613201654515ebb
leon <leon_a_d@yahoo.com>
2015-12-14 15:28:43 +0000

    renaming test helpers

--- test/support/helpers.sh
+++ test/support/helpers.sh
@@ -6,6 +6,6 @@ RESOURCES_DIR="${HELPERS_DIR}/../resources"
 source "${HELPERS_DIR}/load_script.sh"
 source "${HELPERS_DIR}/path.sh" ${SCRIPTS_DIR}
 source "${SCRIPTS_DIR}/functions/messaging.sh"
-source "${HELPERS_DIR}/run_test.sh" ${RESOURCES_DIR}
+source "${HELPERS_DIR}/shunit2.sh" ${RESOURCES_DIR}
 source "${HELPERS_DIR}/find_files.sh"
 source "${SCRIPTS_DIR}/constants.sh"
